3115d7619aabe5ce9206b8978cae768b23311776
BOSTONE069 <bostoneochieng69@gmail.com>
2022-10-11 03:23:07 -0400

    Updated the city and the state objects

--- models/city.py
+++ models/city.py
@@ -3,6 +3,7 @@
 from sqlalchemy import Column, String, ForeignKey
 from models.base_model import BaseModel, Base
 from models import storage_type
+from sqlalchemy.orm import relationship
 
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
@@ -10,6 +11,8 @@ class City(BaseModel, Base):
     if storage_type = 'db':
         name = Column(String(128), nullable=False)
         state_id = Column(String(60), ForeignKey('state.id'), nullable=False)
+        places = relationship('Place', backref='cities',
+                              cascade='all, delete, delete-orphan')
     else:
         state_id = ""
         name = ""
