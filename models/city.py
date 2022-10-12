b908f5553ce8abf034f672d49209f9734118bb21
BOSTONE069 <bostoneochieng69@gmail.com>
2022-10-12 03:59:14 -0400

    updated the city object in the code base

--- models/city.py
+++ models/city.py
@@ -5,10 +5,11 @@ from models.base_model import BaseModel, Base
 from models import storage_type
 from sqlalchemy.orm import relationship
 
+
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
     __tablename__ = 'cities'
-    if storage_type = 'db':
+    if storage_type == "db":
         name = Column(String(128), nullable=False)
         state_id = Column(String(60), ForeignKey('state.id'), nullable=False)
         places = relationship('Place', backref='cities',
