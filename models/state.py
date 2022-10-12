7a3fa31dfb8ac09ebd2df65a4c8b1d8a64a9de7d
BOSTONE069 <bostoneochieng69@gmail.com>
2022-10-12 03:59:43 -0400

    updated the state object in the code base

--- models/state.py
+++ models/state.py
@@ -10,7 +10,7 @@ from sqlalchemy.orm import relationship
 class State(BaseModel):
     """ State class or table model"""
     __tablename__ = 'states'
-    if storage_type = 'db':
+    if storage_type == "db":
         name = Column(String(128), nullable=False)
         cities = relationship('City', backref='state',
                               cascade='all, delete, delete-orphan')
