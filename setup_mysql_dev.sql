4ec8fb867e5d6255e07597897be218110322f810
overwatch <nzioki.grace@gmail.com>
2022-10-12 17:55:25 +0300

    setup db dev and test

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,9 +1,10 @@
 -- Create database and user, grant user permissions without failing if either exists 
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 USE hbnb_dev_db;
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED 
-WITH authentication_plugin 
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED
 BY 'hbnb_dev_pwd';
 
-GRANT PRIVILEGE ON hbnb_dev_db TO 'hbnb_dev'@'localhost';
-GRANT SELECT PRIVILEGE ON performance_schema TO 'hbnb_dev'@'localhost';
+FLUSH PRIVILEGES;
+
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
