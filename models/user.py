31865d282edaddf8bca0fb6ca09bc53fbcae6f2a
BOSTONE069 <bostoneochieng69@gmail.com>
2022-10-13 04:35:52 -0400

    updated the users files

--- models/user.py
+++ models/user.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 """This is the user class"""
 from models.base_model import BaseModel, Base
-from sqlalchemy import String, Column
+from sqlalchemy import String, DateTime, Column, ForeignKey
 from sqlalchemy.orm import relationship
 
 
 class User(BaseModel, Base):
-
     """This is the class for user
     Attributes:
         email: email address
@@ -15,6 +14,7 @@ class User(BaseModel, Base):
         last_name: last name
     """
     __tablename__ = 'users'
+
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=True)
