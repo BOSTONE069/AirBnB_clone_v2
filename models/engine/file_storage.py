25afb4f364e9affa5341932c28460faadef84260
BOSTONE069 <bostoneochieng69@gmail.com>
2022-10-13 03:27:16 -0400

    updated the file storage unit engine

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -12,24 +12,22 @@ class FileStorage:
         """Returns a dictionary of models currently in storage"""
         if cls is None:
             return self.__objects
-        cls_name = cls.__name__
+        class_name = cls.__name__
         dct = {}
         for key in self.__objects.keys():
-            if key.split('.')[0] == cls_name:
+            if key.split('.')[0] == class_name:
                 dct[key] = self.__objects[key]
         return dct
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
-        self.__objects.update(
-            {obj.to_dict()['__class__'] + '.' + obj.id: obj}
-            )
+        self.all().update({obj.to_dict()['__class__'] + '.' + obj.id: obj})
 
     def save(self):
         """Saves storage dictionary to file"""
-        with open(self.__file_path, 'w') as f:
+        with open(FileStorage.__file_path, 'w') as f:
             temp = {}
-            temp.update(self.__objects)
+            temp.update(FileStorage.__objects)
             for key, val in temp.items():
                 temp[key] = val.to_dict()
             json.dump(temp, f)
@@ -45,13 +43,13 @@ class FileStorage:
         from models.review import Review
 
         classes = {
-                    'BaseModel': BaseModel, 'User': User, 'Place': Place,
-                    'State': State, 'City': City, 'Amenity': Amenity,
-                    'Review': Review
-                  }
+            'BaseModel': BaseModel, 'User': User, 'Place': Place,
+            'State': State, 'City': City, 'Amenity': Amenity,
+            'Review': Review
+        }
         try:
             temp = {}
-            with open(self.__file_path, 'r') as f:
+            with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
                     self.all()[key] = classes[val['__class__']](**val)
@@ -59,8 +57,8 @@ class FileStorage:
             pass
 
     def delete(self, obj=None):
-        """ deletes the object obj from the attribute
-            __objects if it's inside it
+        """deletes the obj from the attribute
+        __object if it in the list of
         """
         if obj is None:
             return
@@ -69,5 +67,5 @@ class FileStorage:
             del self.__objects[obj_key]
 
     def close(self):
-        """Call the reload method"""
+        """Calls the reload method"""
         self.reload()
