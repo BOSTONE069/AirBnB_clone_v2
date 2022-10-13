5a66e6c87c576c7dddf7a42d66f72c9dfc49477b
BOSTONE069 <bostoneochieng69@gmail.com>
2022-10-13 04:35:35 -0400

    updated the state file

--- models/state.py
+++ models/state.py
@@ -7,21 +7,21 @@ from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
 
-class State(BaseModel):
-    """ State class or table model"""
+class State(BaseModel, Base):
+    """ State class / table model"""
     __tablename__ = 'states'
-    if storage_type == "db":
+    if storage_type == 'db':
         name = Column(String(128), nullable=False)
         cities = relationship('City', backref='state',
                               cascade='all, delete, delete-orphan')
     else:
-        name = ""
+        name = ''
 
         @property
         def cities(self):
-            """This is to return the list of city instances with state id
-            which equals the current State id
-            FileStorage relationship between State anf City
+            """returns the list of City instances with state_id
+                equals the current State.id
+                FileStorage relationship between State and City
             """
             from models import storage
             related_cities = []
