df90b77f92617ea8199420cd8895af8d7a95a44a
BOSTONE069 <bostoneochieng69@gmail.com>
2022-11-12 16:13:30 -0500

    displays hello HBNB in flask

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-""" Start the flask web framework
+"""
+Start the flask web framework
 The application listens on 0.0.0.0, port 5000
 Routes:
     /:Display 'Hello HBNB'
@@ -12,7 +13,7 @@ app = Flask(__name__)
 @app.route("/", strict_slashes=False)
 def hello_hbnb():
     """Display 'Hello HBNB'"""
-    return 'Hello HBNB'
+    return 'Hello HBNB!'
 
 
 if __name__ == "__main__":
